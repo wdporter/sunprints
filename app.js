b43da535147232caaa02e48313a2b271eaef8e2e
David Porter <david.porter@fastmail.com>
2025-12-17 16:55:42 +1000

    tidy up error handling

--- app.js
+++ app.js
@@ -112,6 +112,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+	console.error(`❌ Error in ${req.method} ${req.path}:`, err);
+
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get("env") === "development" ? err : {}
